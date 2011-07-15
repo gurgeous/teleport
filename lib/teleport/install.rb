084796002634d355673abc7919c2efb9f87879d2
amd <amd@gurge.com>
2011-07-15 12:31:59 -0700

    checkpoint

--- lib/teleport/install.rb
+++ lib/teleport/install.rb
@@ -10,13 +10,45 @@ module Teleport
       _run
     end
 
+    #
+    # public API
+    #
+
+    def user
+      config.user
+    end
+
+    #
+    # private API
+    #
+
     def _run
       run_verbose!
       _finish_ruby_install
       _hostname
+      _create_user
     end
 
     def _finish_ruby_install
+      # fixup 1.8.7
+      ruby_version = `ruby --version`.strip
+      if ruby_version =~ /1.8.7/ && ruby_version !~ /Enterprise Edition/
+        %w(irb libopenssl-ruby libreadline-ruby rdoc ri ruby-dev).each do |i|
+          package_if_necessary(i)
+        end
+        if fails?("which gem")
+          banner "Installing rubygems..."
+          Dir.chdir(DIR) do
+            run "wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.5.tgz"
+            run "tar xfpz rubygems-1.8.5.tgz"
+            Dir.chdir("rubygems-1.8.5") do
+              run "ruby setup.rb"
+            end
+          end
+          ln("/usr/bin/gem1.8", "/usr/bin/gem")
+        end
+      end
+
       # update rubygems if necessary
       gem_version = `gem --version`.strip.split(".").map(&:to_i)
       if (gem_version <=> [1, 8, 5]) == -1
@@ -25,8 +57,7 @@ module Teleport
       end
       
       # uninstall all gems except for bundler
-      gems = `gem list`.split("\n")
-      gems = gems.map { |i| i.split.first }
+      gems = `gem list`.split("\n").map { |i| i.split.first }
       gems.delete("bundler")
       if !gems.empty?
         banner "Uninstalling #{gems.length} system gems..."
@@ -48,12 +79,40 @@ module Teleport
         end
       end
 
-      if `hostname`.strip != config[:host]
-        banner "Setting hostname to #{config[:host]}..."
-        File.open("/etc/hostname", "w") do |f|
-          f.write config[:host]
+      old_hostname = `hostname`.strip
+      return if old_hostname == config[:host]
+      
+      banner "Setting hostname to #{config[:host]} (it was #{old_hostname})..."
+      File.open("/etc/hostname", "w") do |f|
+        f.write config[:host]
+      end
+      run "hostname -F /etc/hostname"
+
+      banner "Fixing up /etc/hosts..."
+      tmppath = "/etc/hosts.tmp"
+      File.open("/etc/hosts") do |fin|
+        File.open(tmppath, "w") do |fout|
+          while line = fin.gets
+            line = line.gsub(/\b#{Regexp.escape(old_hostname)}\b/, config[:host])
+            fout.write line
+          end
         end
-        run "hostname -F /etc/hostname"
+      end
+      mv(tmppath, "/etc/hosts")
+    end
+
+    def _create_user
+      # create the account
+      if !File.directory?("/home/#{user}")
+        banner "Creating #{user} account..."
+        run "useradd --create-home --shell /bin/bash --groups adm #{user}"
+      end
+      if fails?("grep '^#{user}' /etc/sudoers.d/teleport")
+        banner "Setting up sudoers..."
+        File.open("/etc/sudoers.d/teleport", "w") do |f|
+          f.puts "#{user} ALL=(ALL) NOPASSWD: ALL"
+        end
+        chmod("/etc/sudoers.d/teleport", 0440)
       end
     end
   end
