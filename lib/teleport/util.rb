e3d14551b5b524a8de69085b852573c05ea59b2e
dbloete <mail@dennisreimann.de>
2011-11-09 22:39:25 +0100

    Added gem_version method to Util

--- lib/teleport/util.rb
+++ lib/teleport/util.rb
@@ -2,6 +2,7 @@ require "cgi"
 require "digest/md5"
 require "etc"
 require "fileutils"
+require "yaml"
 
 module Teleport
   # Helper module for executing commands and printing stuff
@@ -300,6 +301,18 @@ module Teleport
         return true
       end
       false
+    end
+
+    # Returns the newest currently installed version of
+    # the named gem or false if the gem is not installed
+    def gem_version(name)
+      spec_out = `gem specification #{name} 2> /dev/null`
+      if spec_out != ''
+        spec = Gem::Specification.from_yaml(spec_out)
+        spec.version
+      else
+        false
+      end
     end
 
     # Returns true if the pidfile exists and that process id exists as
